dafaf660055275a5fc1d1c1153754cefa1895763
Philip Victor Jebaraj S <philip.jebaraj@hotmail.com>
2020-07-24 19:50:58 +0530

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ThemeProvider } from "@material-ui/core/styles";
+
+import theme from "./ui/Theme";
+import Header from "../components/ui/Header";
+import Footer from "../components/ui/Footer";
+import LandingPage from "./LandingPage";
+
+function App() {
+  const [value, setValue] = useState(0);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Header
+          value={value}
+          setValue={setValue}
+          selectedIndex={selectedIndex}
+          setSelectedIndex={setSelectedIndex}
+        />
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={(props) => (
+              <LandingPage
+                {...props}
+                setValue={setValue}
+                setSelectedIndex={setSelectedIndex}
+              />
+            )}
+          />
+          <Route exact path="/services" component={() => <div>Services</div>} />
+          <Route
+            exact
+            path="/customsoftware"
+            component={() => <div>Custom Software</div>}
+          />
+          <Route
+            exact
+            path="/mobileapps"
+            component={() => <div>Mobile Apps</div>}
+          />
+          <Route
+            exact
+            path="/websites"
+            component={() => <div>Web Sites</div>}
+          />
+          <Route
+            exact
+            path="/revolution"
+            component={() => <div>Revolution</div>}
+          />
+          <Route exact path="/about" component={() => <div>About Us</div>} />
+          <Route
+            exact
+            path="/contact"
+            component={() => <div>Contact Us</div>}
+          />
+          <Route exact path="/estimate" component={() => <div>Estimate</div>} />
+        </Switch>
+        <Footer
+          value={value}
+          setValue={setValue}
+          selectedIndex={selectedIndex}
+          setSelectedIndex={setSelectedIndex}
+        />
+      </Router>
+    </ThemeProvider>
+  );
+}
+
+export default App;
